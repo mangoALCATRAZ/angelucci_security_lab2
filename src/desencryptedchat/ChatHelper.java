29dfae04e463e030b230e4939690904c1c7c0194
Matt Angelucci? <39205037+mangoALCATRAZ@users.noreply.github.com>
2020-02-21 08:59:56 -0500

    upload

---
+++ src/desencryptedchat/ChatHelper.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desencryptedchat;
+
+/**
+ *
+ * @author Tilley
+ */
+public class ChatHelper {
+    public static String textToBinaryString(String message) {
+        byte[] bytes = message.getBytes();
+        StringBuilder binary = new StringBuilder();
+        for (byte b : bytes) {
+            int val = b;
+            for (int i = 0; i < 8; i++) {
+                binary.append((val & 128) == 0 ? 0 : 1);
+                val <<= 1;
+            }
+            
+        }
+        String bString = binary.toString();
+//        if (bString.length() > 64){
+//            System.out.println("!!!MESSAGE IS LONGER THAN 64 BITS!!!");
+//        }
+        // this is modified
+        while (bString.length() % 64 != 0){
+            bString = "0" + bString;
+        }
+        return bString;
+        
+    }
+    
+    public static String binaryStringToText(String binaryString){
+        String str = "";
+        char nextChar;
+
+        for (int i = 0; i <= binaryString.length() - 8; i += 8) 
+        {
+            nextChar = (char) Integer.parseInt(binaryString.substring(i, i + 8), 2);
+            str += nextChar;
+        }
+        // replaces strange [] character that results from leading zero bytes 
+        String outString = str.replaceAll("[\\p{Cc}\\p{Cf}\\p{Co}\\p{Cn}]", "");
+        
+       
+        
+        return outString;
+    }
+    
+    // splits text into an array of 64 bit strings
+    public static String[] textSplitter(String str){
+//        System.out.println("text to split length:" + str.length());
+        String[] msgArray = new String[str.length() / 64];
+        int index = 0;
+        for (int bit = 0; bit <= str.length() - 64; bit += 64){
+            String str64bit = str.substring(bit, bit + 64);
+            msgArray[index] = str64bit;
+            index += 1;
+        }
+        return msgArray;
+    }
+    
+    public static String keyConverter(String text){
+        String semi = textToBinaryString(text);
+        String k = semi.substring(semi.length() - 64, semi.length());
+        return k;
+        
+    }
+    
+    public static String hmacKeyConverter(String in, int blockSize){
+        String binReadOut = textToBinaryString(in);
+        String binManip = binReadOut;
+        if(binReadOut.length() >= blockSize){
+            return ChatHelper.keyConverter(in);
+        }
+        else{
+            while(binManip.length() < blockSize){
+                binManip.insert
+            }
+        }
+    }
+    
+   
+    
+        
+    
+    
+}
---
+++ src/desencryptedchat/listenerThread.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desencryptedchat;
+
+import java.net.*;
+import java.io.*;
+/**
+ *
+ * @author woah dude
+ */
+public class listenerThread extends Thread{
+    private Socket sock;
+    private volatile boolean endFlag;
+    private String privateKey = "";
+    
+    public listenerThread(Socket inSock){
+        sock = inSock;
+        endFlag = false;
+    }
+    
+    @Override
+    public void run() {
+        try{
+            BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            
+            System.out.println("\nListener Thread Ready.");
+            
+            while(!endFlag){
+                String received = in.readLine(); //incoming ciphertext
+//                System.out.println("received cyphertext: " + received);
+                
+                // DECRYPTION GOES HERE, PRINT OUT RESULTING PLAINTEXT INSTEAD OF received
+                
+                
+                // we might need to allow input of a normal key below
+                
+                String key
+                        = "00010011"
+                        + "00110100"
+                        + "01010111"
+                        + "01111001"
+                        + "10011011"
+                        + "10111100"
+                        + "11011111"
+                        + "11110001";
+                
+                privateKey = DESencryptedChat.getKey();
+                KeyGenerator kg = new KeyGenerator(privateKey);
+                
+                
+                String[] ReversedRoundKeyArray = kg.keyGenerator(kg.getKey());
+                
+//                String[] ReversedRoundKeyArray = KeyGenerator.keyGenerator(KeyGenerator.getKey());
+                
+                ReversedRoundKeyArray = KeyGenerator.roundKeyArrayReversal(ReversedRoundKeyArray);
+                String pt = EncryptDecrypt.Decrypt(received, ReversedRoundKeyArray);
+                String printOut = ChatHelper.binaryStringToText(pt);
+                
+                
+                System.out.println(sock.getInetAddress().toString() + ": " + printOut); // output goes here.
+                
+                
+            }
+            
+            sock.close();
+            System.out.println("Server-aspect done running.");
+
+            
+        }
+        catch(IOException e){
+            
+            System.out.print("\nListener thread: " + e);
+            
+            
+        }
+   }
+    
+    // called by other thread
+    public void end(){
+        endFlag = true;
+    }
+}
+    
+
